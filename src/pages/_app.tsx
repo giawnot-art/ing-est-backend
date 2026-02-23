3d3994ec5bc183bf94cc30d8c7d63b0b60faa148
giawnot-art <giawnot@gmail.com>
2026-02-23 13:55:34 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'; // Usa il percorso relativo invece di @/
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="min-h-screen bg-slate-50">
+       <Component {...pageProps} />
+    </div>
+  );
 }
