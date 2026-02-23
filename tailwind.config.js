1689592b66a3a3b7419241650ea3192a69c66144
giawnot-art <giawnot@gmail.com>
2026-02-23 13:54:59 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        slate: {
-          950: '#020617',
+        ingest: {
+          blue: '#1e40af',
+          dark: '#0f172a',
         }
       }
     },
